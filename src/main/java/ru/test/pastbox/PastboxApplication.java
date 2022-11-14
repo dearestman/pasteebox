bfe1072fce244e4b669c0aaa05545d9f405a664a
Dmitriy Stupakov <dmitriystupakov@MacBook-Pro-Dmitriy.local>
2022-11-14 14:53:29 +0300

    first commit

---
+++ src/main/java/ru/test/pastbox/PastboxApplication.java
@@ -0,0 +1,13 @@
+package ru.test.pastbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/test/pastbox/PastboxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.test.pastbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
